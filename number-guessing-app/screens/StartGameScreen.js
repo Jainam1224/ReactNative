2e720dd47a4420f24d0d65db1f8a73bbf0c8c38e
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-24 22:12:55 +0530

    styling input container and input field

--- number-guessing-app/screens/StartGameScreen.js
+++ number-guessing-app/screens/StartGameScreen.js
@@ -1,11 +1,11 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -13,3 +13,29 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4, // android only property
+    shadowColor: "black", // ios only property
+    shadowOffset: { width: 0, height: 2 }, // ios only property
+    shadowRadius: 6, // ios only property
+    shadowOpacity: 0.25, // ios only property
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
