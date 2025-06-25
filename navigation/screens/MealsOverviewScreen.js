ad32791042ba7461bbb721c486ac27ec1e3dcda6
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-26 02:24:15 +0530

    completing favorite click functionality

--- navigation/screens/MealsOverviewScreen.js
+++ navigation/screens/MealsOverviewScreen.js
@@ -1,7 +1,4 @@
-import { Text, View, StyleSheet, FlatList } from "react-native";
-import { useRoute } from "@react-navigation/native";
 import { CATEGORIES, MEALS } from "../data/dummy-data";
-import MealItem from "../components/MealItem";
 import { useLayoutEffect } from "react";
 
 // We have alternative the route params i.e. useRoute hook - for the screens not registered as a navigation screen
@@ -25,36 +22,7 @@ function MealsOverviewScreen({ route, navigation }) {
     });
   }, [catId, navigation]);
 
-  function renderMealItem(itemData) {
-    const item = itemData.item;
-
-    const mealItemProps = {
-      id: item.id,
-      title: item.title,
-      imageUrl: item.imageUrl,
-      affordability: item.affordability,
-      complexity: item.complexity,
-      duration: item.duration,
-    };
-    return <MealItem {...mealItemProps} />;
-  }
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  );
+  return <MealsList items={displayedMeals} />;
 }
 
 export default MealsOverviewScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
