3fc0177d11aeb429e21099de4301e33b1e721438
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-25 22:27:42 +0530

    displaying meals for each item

--- navigation/screens/MealsOverviewScreen.js
+++ navigation/screens/MealsOverviewScreen.js
@@ -1,14 +1,37 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 // We have alternative the route params i.e. useRoute hook - for the screens not registered as a navigation screen
 // We get navigation and route prop as this is registered as a Navigation screen i.e. Stack.Screen
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
