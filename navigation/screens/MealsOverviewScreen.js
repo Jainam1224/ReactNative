6fabac47d284c7f9126d0057e61d97dccd27df65
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-25 22:13:24 +0530

    route to read the data passed during navigation

--- navigation/screens/MealsOverviewScreen.js
+++ navigation/screens/MealsOverviewScreen.js
@@ -1,9 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
+import { useRoute } from "@react-navigation/native";
 
-function MealsOverviewScreen() {
+// We have alternative the route params i.e. useRoute hook - for the screens not registered as a navigation screen
+// We get navigation and route prop as this is registered as a Navigation screen i.e. Stack.Screen
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview screen</Text>
+      <Text>Meals Overview screen - {catId}</Text>
     </View>
   );
 }
