ebc81dea7e172311c17d35ac4f19e662f817c423
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-25 13:17:39 +0530

    platform specific files

--- number-guessing-app/screens/GameOverScreen.js
+++ number-guessing-app/screens/GameOverScreen.js
@@ -7,7 +7,7 @@ import {
   useWindowDimensions,
 } from "react-native";
 
-import Title from "../components/ui/Title.ios";
+import Title from "../components/ui/Title";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Colors from "../constants/colors";
 
