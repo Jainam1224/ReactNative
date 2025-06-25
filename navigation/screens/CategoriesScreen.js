27e74c112cfed60d54ae067a07be69bd12cc213c
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-25 22:00:26 +0530

    navigation between 2 screens

--- navigation/screens/CategoriesScreen.js
+++ navigation/screens/CategoriesScreen.js
@@ -3,18 +3,27 @@ import CategoryGridTile from "../components/CategoryGridTile.js";
 
 import { CATEGORIES } from "../data/dummy-data";
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
+// navigation => this is given to us by Stack.screen component as a prop
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview");
+    }
 
-function CategoriesScreen() {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
   return (
     <FlatList
       data={CATEGORIES}
       keyExtractor={(item) => item.id}
-      renderItem={renderCategoryItem}
+      renderItem={renderCategoryItem.bind(navigation)}
       numColumns={2}
     />
   );
