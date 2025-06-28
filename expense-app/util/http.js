15fb03f892fd9cf442a83400c6ceddb357b46905
Jainam Sanghvi <jainam.sanghvi@flipkart.com>
2025-06-28 12:09:12 +0530

    add and get expenses from firebase done

--- expense-app/util/http.js
+++ expense-app/util/http.js
@@ -1,8 +1,32 @@
 import axios from "axios";
 
-export function storeExpense(expenseData) {
-  axios.post(
-    "https://expense-app-react-native-60b47-default-rtdb.firebaseio.com/expenses.json",
+const BACKEND_URL =
+  "https://expense-app-react-native-60b47-default-rtdb.firebaseio.com/";
+
+export async function storeExpense(expenseData) {
+  const response = await axios.post(
+    BACKEND_URL + "/expenses.json",
     expenseData
   );
+
+  const id = response.data.name;
+  return id;
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + "/expenses.json");
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
